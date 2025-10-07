5179ad0c749814faba84c89de57dbadfceca5114
zarifbek-yuldashmatov-230251 <161605979+zarifbek-yuldashmatov-230251@users.noreply.github.com>
2025-10-07 14:45:18 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
 using namespace std;
-#include "Lab6.h"
-int main() {
 
-Person Sarvar;
-Sarvar.setName("Sarvar");
-Sarvar.setAge(56);
-    Sarvar.displayPerson();
+#include "Lab_6.h"
+
+int main() {
+    Course oopCourse;
+    oopCourse.setCourseTeacher("Dr. Smith", 40, 2001, "Object-Oriented Programming");
+    //Create students students
+    Student* s1 = new Student("Alice", 20, 1001, 3.8);
+    Student* s2 = new Student("Bob", 22, 1002, 3.6);
+    Student* s3 = new Student("Charlie", 21, 1003, 3.9);
+
+    // Add students to the course
+    oopCourse.addStudent(s1);
+    oopCourse.addStudent(s2);
+    oopCourse.addStudent(s3);
+
+    // Display course details
+    oopCourse.displayCourse();
+
+    // Manual cleanup of aggregated students
+    delete s1;
+    delete s2;
+    delete s3;
+
+
 
     return 0;
 }
